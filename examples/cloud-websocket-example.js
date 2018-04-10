d41d84f1b10f8234400bcd40394e793376e040dc
Gregory Lemercier <g.lemercier@samsung.com>
2018-04-10 09:52:15 +0200

    examples/cloud: reformat tabs into spaces
    Change-Id: I79dccf2dc8e721ef1fc9197f13e148800a0f44a1
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- examples/cloud-websocket-example.js
+++ examples/cloud-websocket-example.js
@@ -11,11 +11,11 @@ var conn = new cloud();
 try{
     opt.setopt("t:d:m:r:sv");
 } catch (e){
-   switch (e.type) {
+    switch (e.type) {
         case "unknown":
             console.log("Unknown option: -%s", e.opt);
             console.log("Usage: node cloud-websocket-example.js [-t <access token>] [-d <device id>] [-m <JSON type test message>]" +
-                        "[-s for enabling SDR (Secure Device Registered) test] [-v for verifying root certificate] [-r <CA root file>]");
+                "[-s for enabling SDR (Secure Device Registered) test] [-v for verifying root certificate] [-r <CA root file>]");
             break;
         case "required":
             console.log("Required parameter for option: -%s", e.opt);
@@ -34,32 +34,32 @@ var ssl_config = {
 
 opt.getopt(function (o, p){
     switch(o){
-    case 't':
-        access_token = String(p);
-        break;
-    case 'd':
-        device_id = String(p);
-        break;
-    case 'm':
-        test_message = String(p);
-        break;
-    case 'r':
-        var data = fs.readFileSync(String(p));
-        ssl_config.ca_cert = Buffer.from(data);
-        break;
-    case 's':
-        ssl_config.se_config = {
-            certificate_identifier: 'artik'
-        }
-        break;
-    case 'v':
-        ssl_config.verify_cert = "required";
-        break;
-    default:
-        console.log("Unknown option: -%s", e.opt);
-        console.log("Usage: node cloud-websocket-example.js [-t <access token>] [-d <device id>] [-m <JSON type test message>]" +
-                    "[-s for enabling SDR (Secure Device Registered) test] [-v for verifying root certificate] [-r <CA root file>]");
-        process.exit(0);
+        case 't':
+            access_token = String(p);
+            break;
+        case 'd':
+            device_id = String(p);
+            break;
+        case 'm':
+            test_message = String(p);
+            break;
+        case 'r':
+            var data = fs.readFileSync(String(p));
+            ssl_config.ca_cert = Buffer.from(data);
+            break;
+        case 's':
+            ssl_config.se_config = {
+                certificate_identifier: 'artik'
+            }
+            break;
+        case 'v':
+            ssl_config.verify_cert = "required";
+            break;
+        default:
+            console.log("Unknown option: -%s", e.opt);
+            console.log("Usage: node cloud-websocket-example.js [-t <access token>] [-d <device id>] [-m <JSON type test message>]" +
+                "[-s for enabling SDR (Secure Device Registered) test] [-v for verifying root certificate] [-r <CA root file>]");
+            process.exit(0);
     }
 });
 
@@ -71,24 +71,24 @@ conn.on('connected', function(result) {
 });
 
 conn.on('receive', function(message) {
-	console.log("Received: " + message);
+    console.log("Received: " + message);
 });
 
 conn.websocket_open_stream(access_token, device_id, ssl_config);
 
 process.on('SIGINT', function () {
-	console.log("Close stream");
-	conn.websocket_close_stream();
-	process.exit(0);
+    console.log("Close stream");
+    conn.websocket_close_stream();
+    process.exit(0);
 });
 
 setInterval(function () {
     console.log("Writing: " + test_message);
-	conn.websocket_send_message(test_message);
+    conn.websocket_send_message(test_message);
 }, 1000);
 
 setTimeout(function() {
-	console.log("Time out, close stream");
-	conn.websocket_close_stream();
-	process.exit(0);
+    console.log("Time out, close stream");
+    conn.websocket_close_stream();
+    process.exit(0);
 }, 5500);
